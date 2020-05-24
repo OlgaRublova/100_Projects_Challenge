b541c76cf6db51e78b3fe210f4c4822c18cb022b
olga <olga.i.rubleva@gmail.com>
2020-05-23 23:16:23 -0700

    finished form

---
+++ base/index.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+// // function expression passed as an argument
+//
+// callMe(function () {
+// //...
+//
+// });
+//
+// // named function expression passed as an argument
+// callMe(function me() {
+// //...
+//
+// });
+//
+// //another function expression
+// var myObject = {
+//     say: function () {
+// //i'm a function expression
+//     }
+// }
